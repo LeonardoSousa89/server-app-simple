2852accb2814719ef658db6c20c9949b254b4806
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-06-12 21:27:44 -0300

    configuracao inicial do index.js

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const express = require('express')
 const cors    = require('cors')
 const app     = express()
 
-
+app.use(cors({ 
+    origin:'*'
+}))
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(express.json())
 app.listen(_Port)
